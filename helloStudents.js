0e3742896685ab6db8bd95c2e1b31460ba3713bc
Christine Nuq <cnuqui@icloud.com>
2023-01-15 22:54:14 -0500

    in-class-1

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,19 @@
 'use strict'
 
 // 1. Read the JSON file into a variable called students
+const students = require("./students.json");
+console.log(students);
 
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
 
+students.forEach((student) => {
+    console.log("Hello " + student.firstName + " " + student.lastName);
+});
+
+
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+const lastNameD = students.filter(({lastName}) => lastName.charAt(0).toUpperCase === "D");
+    console.log(`Count of last names starting with D is ${lastNameD.length}`);
